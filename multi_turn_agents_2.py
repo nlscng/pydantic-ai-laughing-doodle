46e186a0cd8c4c84cffa1c99eac41163b8ba2d58
ncheng <nlscng.g@gmail.com>
2025-07-16 21:42:29 -0500

    Sample multi turn agents with message_history passing.

---
+++ multi_turn_agents_2.py
@@ -0,0 +1,15 @@
+## Based on example from pydantic_ai documentation
+## https://ai.pydantic.dev/message-history/#storing-and-loading-messages-to-json
+from pydantic_ai import Agent
+
+agent = Agent('openai:gpt-4o', system_prompt='Be a helpful assistant.')
+
+result1 = agent.run_sync('Tell me a joke.')
+print(result1.output)
+#> Did you hear about the toothpaste scandal? They called it Colgate.
+
+result2 = agent.run_sync('Explain?', message_history=result1.new_messages())
+print(result2.output)
+#> This is an excellent joke invented by Samuel Colvin, it needs no explanation.
+
+print(result2.all_messages())
